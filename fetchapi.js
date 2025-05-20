7d65332e1577fea8c34e297bef1b052e600bb9db
Noel Odero <anoel.odero@gmail.com>
2025-05-20 13:20:38 +0200

    custom headers

--- fetchapi.js
+++ fetchapi.js
@@ -42,83 +42,83 @@ async function fetchPosts(){
 fetchPosts()
 
 
-// // fetch using query params
+// fetch using query params
 
-// async function fetchPostsWithComments(){
-//     const API_URL =  "https://localhost:3000/posts";
-//     const queryPArams = {
-//         _embed: "comments",
-//     }
+async function fetchPostsWithComments(){
+    const API_URL =  "https://localhost:3000/posts";
+    const queryPArams = {
+        _embed: "comments",
+    }
    
-//     try {
-//         const queryString = new URLSearchParams(queryParams).toString();
-//         const url = `${API_URL}${queryString}`
-//         const res = await fetch(url);
-//         const data = await res.json();
-//         console.log(data)
+    try {
+        const queryString = new URLSearchParams(queryParams).toString();
+        const url = `${API_URL}${queryString}`
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data)
 
-//     } catch(error){
-//         console.log(error)
+    } catch(error){
+        console.log(error)
 
-//     }
-// }
+    }
+}
 
-// // fetchPostsWithComments()
-
-
-
-// // fetch usage: creating resource
-
-// async function createPost(postDAta){
-//     const API_URL = "https://jsonplaceholder.typicode.com/todos";
-//     try {
-//        const res =  await fetch(API_URL, {
-//             method: "POST",
-//             headers: {"Content-Type": "application/json"},
-//             body: JSON.stringify(postDAta)
-//         });
-//         const result = await res.json();
-//         console.log(result)
-
-//     } catch(error){
-//         console.log(error)
-//     }
-// }
-// const newPostData = {
-//     userId: 101,
-//     id: 101,
-//     title: 'new post!',
-//     completed: 'true'
-// }
-// createPost(newPostData)
+// fetchPostsWithComments()
 
 
-// // Updating an entire resource
+
+// fetch usage: creating resource
+
+async function createPost(postDAta){
+    const API_URL = "https://jsonplaceholder.typicode.com/todos";
+    try {
+       const res =  await fetch(API_URL, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(postDAta)
+        });
+        const result = await res.json();
+        console.log(result)
+
+    } catch(error){
+        console.log(error)
+    }
+}
+const newPostData = {
+    userId: 101,
+    id: 101,
+    title: 'new post!',
+    completed: 'true'
+}
+createPost(newPostData)
 
 
-// async function updatePost(postData) {
-//     console.log('updating data')
-//     const url = "https://jsonplaceholder.typicode.com/todos/101";
-//     try {
-//         const res = await fetch(url, {
-//             method: "PUT",
-//             headers: {"Content-Type": "application/json"},
-//             body: JSON.stringify(postData)
-//         })
-//         const data = await res.json();
-//         console.log(data)
-//     } catch(error) {
-//         console.log(error)
-//     }
-// }
-// const postData = {
-//     userId: 101,
-//     id: 101,
-//     title: 'new post now!',
-//     completed: 'false'
-// }
+// Updating an entire resource
 
-// updatePost(postData)
+
+async function updatePost(postData) {
+    console.log('updating data')
+    const url = "https://jsonplaceholder.typicode.com/todos/101";
+    try {
+        const res = await fetch(url, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(postData)
+        })
+        const data = await res.json();
+        console.log(data)
+    } catch(error) {
+        console.log(error)
+    }
+}
+const postData = {
+    userId: 101,
+    id: 101,
+    title: 'new post now!',
+    completed: 'false'
+}
+
+updatePost(postData)
 
 
 // delete post
@@ -143,4 +143,31 @@ async function deleteData(postId) {
 deleteData(1);
 
 // custom headers
+//  To make a post, patch or put, you pass headers like application/json. If you are dealing with authentication for example, you use authorization header
+
+async function login(){
+    const API_URL = "http://localhost:3000/login";
+
+    try{
+        const res = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Authorization: "Bearer secret-token",
+                "Custom-header": "Learn fetch api"
+            },
+            body: JSON.stringify({Username: "missnoel", password: "password"})
+        })
+        const data = await res.json();
+        console.log(data)
+
+    } catch(err){
+        console.log(err)
+    }
+}
+
+login()
+
+// Creating a request object
+
 
